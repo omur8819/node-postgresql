fa19c8a2b90c1031ee94bfa8988138a74ae3597b
omur8819 <omur8819@gmail.com>
2020-12-10 15:51:34 +0300

    Sequelize added

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const { Sequelize } = require("sequelize");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
+//connect to db
+const sq = new Sequelize('localhost::5432:');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
